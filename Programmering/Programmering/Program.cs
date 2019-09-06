d1b358d66a013ab2b177f5b09786278f5e942cde
Maximiliam <maxmaonen@gmail.com>
2019-09-06 09:26:22 +0200

    xD

---
+++ Programmering/Programmering/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Programmering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Johan<Max");
+        }
+    }
+}
